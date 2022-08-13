1026dcf786657e85acaef8f5f008c6d1d5ba3a96
banjipeng <ban_jipeng@163.com>
2022-08-13 20:44:01 +0800

    merge into master environment

--- private-project/src/main/java/org/huawei/rms/ForkJoinTask.java
+++ private-project/src/main/java/org/huawei/rms/ForkJoinTask.java
@@ -4,6 +4,7 @@ public class ForkJoinTask {
 
     public static void main(String[] args) {
         System.out.println("test dev environment");
+        System.out.println("merge into master environment");
     }
 
 
